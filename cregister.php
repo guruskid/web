2946aa6f929a96f8529dd624e1d873de526138fd
Bright Robert <guruskid@yahoo.com>
2018-07-10 21:11:22 +0100

    Fix

---
+++ cregister.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>COMPLETE REGISTRATION</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="//" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<!-- pop-up-box -->
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //pop-up-box -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<link href="//fonts.googleapis.com/css?family=Prata" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+	
+<body>
+<!-- header -->
+	<div class="header-w3ls-agileinfo">
+		<div class="wthree_agile_top_header">
+			<div class="logo-agileits">
+				<h1><a href="index.html"><span>Student</span>Fund <i class="fa fa-graduation-cap" aria-hidden="true"></i></a></h1>
+			</div>
+			<div class="agileits_w3layouts_sign_in">
+				<ul>
+					<li><a class="sign" href="#" data-toggle="modal" data-target="#myModal2" >Sign In</a></li>
+					<li><a class="signup" href="#" data-toggle="modal" data-target="#myModal3" >Sign Up</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="container">
+			<div class="w3layouts_agileits_nav_section">
+				<nav class="navbar navbar-default">
+					<div class="navbar-header navbar-left">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="w3ls__agileinfo_search">
+						<form action="#" method="post">
+							<input type="search" name="Search" placeholder="Search here..." required="">
+							<input type="submit" value=" ">
+						</form>
+					</div>
+					<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+						<nav>
+							<ul class="nav navbar-nav">
+								<li><a href="./" class="hvr-rectangle-out">Home</a></li>
+								<li><a href="path" class="hvr-rectangle-out">Path</a></li>
+								<li><a href="sponsor" class="hvr-rectangle-out">Sponsor</a></li>
+								<li class="active"><a href="contact">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</nav>	
+			</div>
+		</div>
+	</div>
+		<!-- //header -->
+		<!-- Modal1 -->
+		<div class="modal fade" id="myModal2" tabindex="-1" role="dialog">
+			<div class="modal-dialog">
+				<!-- Modal content-->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<div class="signin-form profile">
+							<h3 class="agileinfo_sign">Sign In</h3>	
+							<div class="login-form">
+								<form action="#" method="post">
+									<input type="text" name="email" placeholder="E-mail" required="">
+									<input type="password" name="password" placeholder="Password" required="">
+									<div class="tp">
+										<input type="submit" value="Sign In">
+									</div>
+								</form>
+							</div>
+							<div class="login-social-grids">
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+								</ul>
+							</div>
+							<p><a href="#" data-toggle="modal" data-target="#myModal3" > Don't have an account?</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- //Modal1 -->	
+		<!-- Modal2 -->
+		<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+			<div class="modal-dialog">
+				<!-- Modal content-->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<div class="signin-form profile">
+							<h3 class="agileinfo_sign">Sign Up</h3>
+							<div class="login-form">
+								<form action="#" method="post">
+									<input type="text" name="name" placeholder="Username" required="">
+									<input type="email" name="email" placeholder="Email" required="">
+									<input type="password" name="password" placeholder="Password" required="">
+									<input type="password" name="password" placeholder="Confirm Password" required="">
+									<input type="submit" value="Sign Up">
+								</form>
+							</div>
+							<p><a href="#"> By clicking register, I agree to your terms</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- //Modal2 -->
+		<hr>	
+			           <!--  Login Form -->
+           <section>
+            <div class="container cregister">
+              <div class="title"><h2>COMPLETE REGISTRATION</h2></div>
+              <div class="formBox form-group">
+                <form action="">
+                  <div class="left one">
+                    <span style="color:red">Note: Please select only image file (eg: .png, .jpeg, .jpg,)<br/>Max File size : 1MB allowed </span><br>
+                    <input type='file' id="file" name="upload" required="">
+                    <script src="js/upload.js"></script>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="left form-group">
+                    <p>First Name</p>
+                    <input type="text" disabled="" name="firstname" placeholder="First Name">
+                  </div>
+                  <div class="right form-group">
+                    <p>Last Name</p>
+                    <input type="text" disabled="" name="lastname" placeholder="Last Name">
+                  </div>
+                  <div class="left form-group">
+                    <p>Email:</p>
+                    <input type="email" disabled="" name="email" placeholder="Email Address"> 
+                  </div>
+                  <div class="right form-group">
+                    <p>Phone Number:</p>
+                    <input type="number" name="number" required="" placeholder="+234 080">
+                  </div>
+                  <div class="left one form-group">
+                    <p>Gender: </p>
+                    <select name="" id="" required="">
+                      <option disabled="" selected="">Select Gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                  </div>
+                  <div class="right one form-group">
+                    <p>Marital Status: </p>
+                    <select name="" id="" required="">
+                      <option value="" disabled="" selected="">Select Status</option>
+                      <option value="single">Single</option>
+                      <option value="married">Married</option>
+                      <option value="divorce">Divorce</option>
+                    </select>
+                  </div>
+                  <div class="account form-group"><h5>Academy Detail</h5></div>
+                  <div class="left">
+                    <p>Matric Number</p>
+                    <input type="text" name="matric" required="" placeholder="Matric Number">
+                  </div>
+                  <div class="right form-group">
+                    <p>Year Admitted</p>
+                    <input type="number" name="admityear" required="" placeholder="Admit Year">
+                  </div>
+                  <div class="left form-group">
+                    <p>Course / Department</p>
+                    <input type="text" name="course" required="" placeholder="Course / Department">
+                  </div>
+                  <div class="right two form-group">
+                    <p>University / Polytechnic</p>
+                    <select name="" id="" required="">
+                      <option value="">Select University / Polytechnic</option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                    </select>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="left form-group">
+                    <p>Academy Level</p>
+                    <select name="" id="" required="">
+                      <option value="" selected="" disabled="">Select Academy Level</option>
+                      <option value="">100 Level</option>
+                      <option value="">200 Level</option>
+                      <option value="">300 Level</option>
+                      <option value="">400 Level</option>
+                      <option value="">500 level</option>
+                    </select>
+                  </div>
+                  <div class="right form-group">
+                    <p>Sponsor</p>
+                    <select name="" id="" required="">
+                      <option value="" disabled="" selected="">Select Sponsor</option>
+                      <option value="">Parents</option>
+                      <option value="">Guidance</option>
+                      <option value="">Self</option>
+                    </select>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="account"><h5>Account Detail</h5></div>
+                  <div class="left form-group">
+                    <p>Bank:</p>
+                    <select name="" id="" required="">
+                      <option value="" selected="" disabled="">Bank</option>
+                      <option value="">First Bank</option>
+                    </select>
+                  </div>
+                  <div class="right one form-group">
+                    <p>Account Type</p>
+                    <select name="" id="" required="">
+                      <option value="" selected="" disabled="">Select Account Type</option>
+                      <option value="">Saving</option>
+                      <option value="">Current</option>
+                      <option value="">Checking</option>
+                    </select>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="left form-group">
+                    <p>Account Name:</p>
+                    <input type="text" name="accountname" required="" placeholder="Account Name">
+                  </div>
+                  <div class="right form-group">
+                    <p>Account Number:</p>
+                    <input type="number"  name="accountnumber" required="" placeholder="Account Number">
+                  </div>
+                  <input type="submit" name="" value="PROCEED">
+                  <a href="#">Already Have Account ?</a>
+                </form>
+              </div>
+            </div>
+           </section>
+           <!-- End Login Form -->
+			<!-- Footer -->
+			<div class="footer w3ls">
+				<div class="container">
+					<div class="footer-main">
+						<div class="copyrights">
+							<p>&copy;  All Rights Reserved</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Footer -->	
+			<!-- start-smoth-scrolling -->
+			<!-- js -->
+			<!--//pop-up-box -->
+			<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+			<!--pop-up-box -->
+			<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+			<script>
+				$(document).ready(function() {
+					$('.popup-with-zoom-anim').magnificPopup({
+						type: 'inline',
+						fixedContentPos: false,
+						fixedBgPos: true,
+						overflowY: 'auto',
+						closeBtnInside: true,
+						preloader: false,
+						midClick: true,
+						removalDelay: 300,
+						mainClass: 'my-mfp-zoom-in'
+					});
+				});
+			</script>
+			<!-- //pop-up-box -->
+			<!-- //js -->
+			<script type="text/javascript" src="js/move-top.js"></script>
+			<script type="text/javascript" src="js/easing.js"></script>
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					$(".scroll").click(function(event){
+						event.preventDefault();
+						$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+					});
+				});
+			</script>
+			<!-- start-smoth-scrolling -->
+			<script src="js/jzBox.js"></script>
+			<!-- Countdown-Timer-JavaScript -->
+			<script src="js/simplyCountdown.js"></script>
+			<script>
+				var d = new Date(new Date().getTime() + 948 * 120 * 120 * 2000);
+
+				// default example
+				simplyCountdown('.simply-countdown-one', {
+					year: d.getFullYear(),
+					month: d.getMonth() + 1,
+					day: d.getDate()
+				});
+
+				// inline example
+				simplyCountdown('.simply-countdown-inline', {
+					year: d.getFullYear(),
+					month: d.getMonth() + 1,
+					day: d.getDate(),
+					inline: true
+				});
+
+				//jQuery example
+				$('#simply-countdown-losange').simplyCountdown({
+					year: d.getFullYear(),
+					month: d.getMonth() + 1,
+					day: d.getDate(),
+					enableUtc: false
+				});
+			</script>
+		<!-- //Countdown-Timer-JavaScript -->
+		<!-- smooth scrolling -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+			/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+			*/								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<!-- //smooth scrolling -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
